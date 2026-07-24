29cd4bb70d7ad8fbe79eb91b7b47316055dfdd07
mohmmadkazali3-collab <mohmmadkazali3@gmail.com>
2026-07-24 12:56:18 +0300

    Update Netlify configuration

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,7 +1,7 @@
 /** Edit these values before deploying. */
 export const CONFIG = {
   whatsapp: "962779060506", // TODO: international format, no + or leading zeros
-  email: "info@nexora.agency", // TODO
+  email: "info@nexora-agency.tech", // TODO
   instagram: "https://instagram.com/your.handle", // TODO
   linkedin: "https://linkedin.com/company/your-handle" // TODO
 };
